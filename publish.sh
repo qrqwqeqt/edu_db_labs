d286ce58ea5fa11fc870cd12ef7b5c3109ac181a
Oleksii <playmega07@gmail.com>
2023-12-27 22:15:52 +0200

    6

--- publish.sh
+++ publish.sh
@@ -16,6 +16,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:boldak/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/Kkrinch/edu_db_labs.git master:gh-pages
+git push -f https://github.com/Kkrinch/edu_db_labs master:gh-pages
 
 cd -
